47c66fb35645eb2b4f3a3c0d0c09e27895f515b4
Admin <Admin@192.168.2.113>
2021-02-24 17:36:26 +0530

    Added script code

--- Demo/src/main/java/testcases/TC003.java
+++ Demo/src/main/java/testcases/TC003.java
@@ -2,12 +2,16 @@ package testcases;
 
 import org.testng.annotations.Test;
 
+import pages.Homepage;
+
 public class TC003 extends BaseTest 
 {
 	@Test
 	public void createUser() 
 	{
-		
+		Homepage homepage=new Homepage(driver, webActionUtil);
+		homepage.enterKeysInSearchBox("Tomato");
+		homepage.clickOnSearchButton();
 		
 	}
 }
